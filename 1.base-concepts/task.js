c5c6037ec706c9fd8aa287619fcf5ad6a3ebefdb
Artur Agliullin <artur_agliullin@mail.ru>
2023-08-11 00:57:12 +0500

    homework 1 is done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discr = b**2 - 4*a*c;
+  if (discr === 0) {
+    arr.push(-b/2*a)
+  }
+  else if (discr > 0) {
+    arr.push((-b + Math.sqrt(discr) )/(2*a));
+    arr.push((-b - Math.sqrt(discr) )/(2*a));
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthly_percent = (percent/100)/12;
+  let body = amount - contribution;
+  let payment = body * (monthly_percent + (monthly_percent / (((1 + monthly_percent)**countMonths) - 1)));
+  let result = payment * countMonths;
+  return Number(result.toFixed(2));
 }
